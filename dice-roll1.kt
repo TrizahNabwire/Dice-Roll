879d7f3d8c57fd729de00bf89cdf8e0104c73181
Trizah Nabwire <99127888+TrizahNabwire@users.noreply.github.com>
2022-02-10 17:24:16 +0300

    Update dice-roll1.kt

--- dice-roll1.kt
+++ dice-roll1.kt
@@ -1,5 +1,5 @@
 fun main(){
-  val diceRange = 1..6 # read as val diceRange: IntRange = 1..6
+  val diceRange = 1..6 // read as val diceRange: IntRange = 1..6
   val randomNumber = diceRange.random()
   println("Random Number : ${randomNumber}")
 }
